c4b0a3a9aa5d4127ff7e250801fbfa5eea38b20e
rhutvika <rhutvikaekbote16@gmail.com>
2019-09-27 09:35:45 -1200

    abstraction

---
+++ machine/src/machine/TestMachine.java
@@ -0,0 +1,26 @@
+package machine;
+
+public class TestMachine {
+
+	public static void main(String[] args) {
+		machine []m=new machine[10];
+		m[0]=new Tables();
+		m[1]=new Syrup();
+		m[2]=new Ointment();
+		m[3]=new Ointment();
+		m[4]=new Tables();
+		m[5]=new Syrup();
+		m[6]=new Tables();
+		m[7]=new Syrup();
+		m[8]=new Ointment();
+		m[9]=new Syrup();
+		Object i=null;
+		display (i);
+		}
+
+	private static void display(Object i) {
+		
+		
+	}
+
+}
---
+++ machine/src/machine/machine.java
@@ -0,0 +1,25 @@
+package machine;
+
+public abstract class machine {
+	
+	public void displaylabel()
+	{
+	}
+}
+class Tables extends machine{
+	public void displaylabel() {
+		
+	}
+}
+
+class Syrup extends machine{
+	public void displaylabel() {
+		
+	}
+}
+
+class Ointment extends machine{
+	public void displaylabel() {
+		
+	}
+}
